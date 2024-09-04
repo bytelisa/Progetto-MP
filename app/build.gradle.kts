d0df5bd69c3f63243d8058a9df06a2269656cc19
valejin <valentina.jin@students.uniroma2.eu>
2024-09-04 16:10:36 +0200

    connessione DB

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+
 }
 
 android {
@@ -66,4 +67,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.xerial:sqlite-jdbc:3.42.0.0") // Versione di sqlite-jdbc
 }
