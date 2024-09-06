80e271042247b7b150cd904fe1d2f28dc155f55a
bytelisa <elisa.marzioli2002@gmail.com>
2024-09-06 15:59:31 +0200

    prima bozza Home

--- app/src/main/java/it/VES/yahtzee/MainActivity.kt
+++ app/src/main/java/it/VES/yahtzee/MainActivity.kt
@@ -4,16 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import it.VES.yahtzee.bluebutton.BlueButton
+import androidx.compose.ui.unit.sp
 import it.VES.yahtzee.ui.theme.YahtzeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,10 +43,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             YahtzeeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Sofia",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
@@ -35,33 +52,97 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column(){
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-        BlueButton(
+fun Home() {
+
+        Column(
             modifier = Modifier
-                .padding(horizontal = 16.dp, vertical = 8.dp),
-            onClick = {
-                //SimpleTextDisplay("ciaooo")
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ){
+
+            Text(
+                text = "Yahtzee!",
+                //fontStyle = FontStyle.Normal.,
+                fontSize = 40.sp, // Big font size
+                fontWeight = FontWeight.Bold, // Bold text
+                textAlign = TextAlign.Center, // Center the text
+                color = Color(0xFF009688), // Custom color (purple)
+                modifier = Modifier.padding(top = 24.dp) // Add some padding from the top
+            )
+            // Space between title and buttons
+            Spacer(modifier = Modifier.height(200.dp))
+
+            Button(
+                onClick = { /*TODO: Action*/ },
+                modifier = Modifier
+                    .width(200.dp) // Set width for all buttons
+                    .height(45.dp), // Set height for all buttons)
+            ) {
+                Text(text = "Play")
             }
-        )
+            // Space between title and buttons
+            Spacer(modifier = Modifier.height(24.dp))
+
+            // Second Button
+            Button(
+                onClick = { /*TODO: Action*/ },
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(45.dp),
+                ) {
+                Text(text = "How to Play")
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            // Third Button
+            Button(
+                onClick = { /*TODO: Action*/ },
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(45.dp),
+            ) {
+                Text(text = "Settings")
+            }
+            Spacer(modifier = Modifier.height(24.dp))
+
+            UserNameInput()
+        }
 
     }
 
-}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HomePreview() {
     YahtzeeTheme {
-        Greeting("Android")
+        Home()
     }
 }
 
 @Composable
 fun SimpleTextDisplay(message: String) {
     Text(text = message)
+}
+
+@Composable
+fun Play() {
+
+}
+
+@Composable
+fun UserNameInput() {
+
+    // Stato per memorizzare il testo inserito
+    var text by rememberSaveable { mutableStateOf("") }
+
+    TextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text("Inserisci il tuo nome") },
+        modifier = Modifier.fillMaxWidth(),
+        placeholder = {Text("Nome")}
+    )
 }
